a9a9891a93a13e50a5160bfc53d7b34f45023044
JetsadaWijit2 <122506976+JetsadaWijit2@users.noreply.github.com>
2025-06-25 10:44:49 +0000

    chore: Delete DB system

--- src/main/java/io/github/mcengine/api/currency/database/mysql/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains services that integrate with MySQL.
- */
-package io.github.mcengine.api.currency.database.mysql;
--- src/main/java/io/github/mcengine/api/currency/database/sqlite/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains services that integrate with SQLite.
- */
-package io.github.mcengine.api.currency.database.sqlite;
